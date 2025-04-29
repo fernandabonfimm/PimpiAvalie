485acf345b33ddd54d4722fb990ea21433c9a710
Mguell <mguellntr@gmail.com>
2025-04-28 21:06:08 -0300

    desenvolvendo a validacao

--- front/src/pages/step1Avaliacao.jsx
+++ front/src/pages/step1Avaliacao.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Layout, Form, Input } from "antd";
 import Logo from "../assets/images/logo3.png";
-import { Layout } from "antd";
-const { Content, Footer } = Layout;
-import "../styles/pages.css";
 import StepsComponent from "../components/steps";
+import "../styles/pages.css";
+
+const { Content } = Layout;
+
 const Step1Validacao = () => {
+  const [form] = Form.useForm();
+
+  const validateNome = (string) => {
+    if (!value) {
+      return Promise.reject(new Error("Por favor, insira seu nome"));
+    }
+    const regexOnlyNumbers = /^[0-9\s]+$/;
+    if (regexOnlyNumbers.test(value)) {
+      return Promise.reject(new Error("Nome não pode conter apenas números"));
+    }
+    return Promise.resolve();
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const values = await form.validateFields();
+      console.log("Success:", values);
+      alert("Avaliação Concluída!");
+    } catch (errorInfo) {
+      console.log("Failed:", errorInfo);
+    }
+  };
+
   return (
     <>
       <Content className="container-step1-avalie">
@@ -12,8 +37,67 @@ const Step1Validacao = () => {
           <img src={Logo} alt="logo" />
         </div>
         <StepsComponent
-          onClickEnd={() => alert("Avaliação Concluída!")}
-          children={<></>}
+          onClickEnd={handleSubmit}
+          children={
+            <Form
+              layout="vertical"
+              form={form}
+              className="form-avalie"
+              initialValues={{
+                nome: "",
+                email: "",
+                celular: "",
+                localCompra: "",
+                cidade: "",
+              }}
+            >
+              <Form.Item
+                label="Nome"
+                name="nome"
+                rules={[
+                  { required: true, message: "Por favor, insira seu nome" },
+                  { validator: validateNome },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  { required: true, message: "Por favor, insira seu email" },
+                  { type: "email", message: "Por favor, insira um email válido" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Celular"
+                name="celular"
+                rules={[{ required: true, message: "Por favor, insira seu celular" }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Local da Compra"
+                name="localCompra"
+                rules={[{ required: true, message: "Por favor, informe o local da compra" }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Cidade"
+                name="cidade"
+                rules={[{ required: true, message: "Por favor, informe sua cidade" }]}
+              >
+                <Input />
+              </Form.Item>
+            </Form>
+          }
         />
       </Content>
     </>
