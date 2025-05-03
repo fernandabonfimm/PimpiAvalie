c76b0845d33bef157725b054b63323818c6b9ddb
AyslanSouza <ayslans75@gmail.com>
2025-05-03 10:49:04 -0300

    Ajustes no css

--- front/src/pages/step1Avaliacao.jsx
+++ front/src/pages/step1Avaliacao.jsx
@@ -2,14 +2,16 @@ import React, { useState } from "react";
 import { Layout, Form, Input } from "antd";
 import Logo from "../assets/images/logo3.png";
 import StepsComponent from "../components/steps";
-import "../styles/pages.css";
+import "../styles/pages_avalie.css";
+
+
 
 const { Content } = Layout;
 
 const Step1Validacao = () => {
   const [form] = Form.useForm();
 
-  const validateNome = (string) => {
+  const validateNome = (_, value) => {
     if (!value) {
       return Promise.reject(new Error("Por favor, insira seu nome"));
     }
@@ -20,13 +22,39 @@ const Step1Validacao = () => {
     return Promise.resolve();
   };
 
+  // const handleSubmit = async () => {
+  //   try {
+  //     const values = await form.validateFields();
+  //     console.log("Success:", values);
+  //     alert("Avaliação Concluída!");
+  //   } catch (errorInfo) {
+  //     console.log("Failed:", errorInfo);
+  //   }
+  // };
   const handleSubmit = async () => {
     try {
       const values = await form.validateFields();
       console.log("Success:", values);
-      alert("Avaliação Concluída!");
-    } catch (errorInfo) {
-      console.log("Failed:", errorInfo);
+  
+      // Envio dos dados para o backend
+      const response = await fetch("https://coloca o link do backend", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+  
+      if (!response.ok) {
+        throw new Error("Erro ao enviar dados");
+      }
+  
+      const data = await response.json();
+      console.log("Resposta do backend:", data);
+      alert("Avaliação enviada com sucesso!");
+    } catch (error) {
+      console.error("Erro no envio:", error);
+      alert("Erro ao enviar a avaliação. Tente novamente.");
     }
   };
 
